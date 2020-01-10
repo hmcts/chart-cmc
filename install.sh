6c778148d784816d45921e034425324b138d3244
Karim Lawani <k7lawani@gmail.com>
2020-01-10 16:11:26 +0000

    update config

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #  --install --wait --timeout ${2} \
 #  --namespace money-claims-test
 
-helm upgrade cmc-test cmc \
-  -f cmc/values.yaml -f ci-values.yaml \
+helm upgrade chart-cmc-release cmc \
+  -f cmc/values.yaml -f cmc/values.preview.template.yaml \
   --install --wait --timeout 600 \
   --namespace money-claims
