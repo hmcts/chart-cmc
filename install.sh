e7fadd980ab5e7ec396eeccc033eba8facffc346
Karim Lawani <k7lawani@gmail.com>
2020-01-10 15:48:28 +0000

    update ci-values

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-helm upgrade demo-v1 cmc \
-  -f cmc/values.yaml -f cmc/${1}.yaml \
-  --install --wait --timeout ${2} \
-  --namespace money-claims-test
+#!/bin/sh
+#helm upgrade demo-v1 cmc \
+#  -f cmc/values.yaml -f cmc/${1}.yaml \
+#  --install --wait --timeout ${2} \
+#  --namespace money-claims-test
+
+helm upgrade cmc-test cmc \
+  -f cmc/values.yaml -f ci-values.yaml \
+  --install --wait --timeout 600 \
+  --namespace money-claims
