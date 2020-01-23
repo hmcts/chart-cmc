2247234da0f45215e8f5f939e56bc1497cb77339
Karim Lawani <k7lawani@gmail.com>
2020-01-23 15:13:05 +0000

    testing blob storage

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-#helm upgrade demo-v1 cmc \
-#  -f cmc/values.yaml -f cmc/${1}.yaml \
-#  --install --wait --timeout ${2} \
-#  --namespace money-claims-test
-
 helm upgrade chart-cmc-preview cmc \
-  -f cmc/values.yaml -f cmc/values.preview.template.yaml \
-  --install --wait --timeout 900 \
+  -f cmc/values.yaml -f cmc/values.preview.yaml \
+  --install --wait --timeout 900s \
   --namespace money-claims
