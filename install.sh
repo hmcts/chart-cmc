a91081b1fb51e17115db0f83b5a043c7b4e08ff9
Karim Lawani <k7lawani@gmail.com>
2020-01-23 11:48:45 +0000

    start enabling blob storage

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-helm upgrade demo-v1 cmc \
-  -f cmc/values.yaml -f cmc/${1}.yaml \
-  --install --wait --timeout ${2} \
-  --namespace money-claims-test
+#!/bin/sh
+
+#helm upgrade demo-v1 cmc \
+#  -f cmc/values.yaml -f cmc/${1}.yaml \
+#  --install --wait --timeout ${2} \
+#  --namespace money-claims-test
+
+helm upgrade chart-cmc-preview cmc \
+  -f cmc/values.yaml -f cmc/values.preview.template.yaml \
+  --install --wait --timeout 900 \
+  --namespace money-claims
