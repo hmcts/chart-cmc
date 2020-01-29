16666e609897c20b81b0b4de1c5e37f01b62404a
Karim Lawani <k7lawani@gmail.com>
2020-01-29 09:50:43 +0000

    refactor cmc chart

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-helm upgrade chart-cmc-preview cmc \
+helm upgrade cmc-preview cmc \
   -f cmc/values.yaml -f cmc/values.preview.yaml \
-  --install --wait --timeout 900s \
+  --install --wait --timeout 500s \
   --namespace money-claims \
   --set global.subscriptionId=1c4f0704-a29e-403d-b719-b9
