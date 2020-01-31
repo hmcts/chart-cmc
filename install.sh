7d24e0ec3cab138f0c683e81b1e0e5b119a16ca2
Karim Lawani <k7lawani@gmail.com>
2020-01-31 13:47:53 +0000

    Refactor/cmc cleanup (#41)

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-helm upgrade chart-cmc-preview cmc \
+helm upgrade cmc-preview cmc \
   -f cmc/values.yaml -f cmc/values.preview.yaml \
-  --install --wait --timeout 900s \
-  --namespace money-claims
+  --install --wait --timeout 500s \
+  --namespace money-claims \
+  --set global.subscriptionId=1c4f0704-a29e-403d-b719-b9
